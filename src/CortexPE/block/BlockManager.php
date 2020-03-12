6464117b698a1c6cffb3be0d0057d96cf22cfeb0
Invy55 <38048104+Invy55@users.noreply.github.com>
2020-03-12 15:10:55 +0100

    Update BlockManager.php

--- src/CortexPE/block/BlockManager.php
+++ src/CortexPE/block/BlockManager.php
@@ -67,6 +67,5 @@ class BlockManager {
 		BlockFactory::registerBlock(new Rail(), true);
 		BlockFactory::registerBlock(new Cauldron(), true);
 		//BlockFactory::registerBlock(new Jukebox(), true);
-        BlockFactory::registerBlock(new Sponge(), true);
 	}
 }
